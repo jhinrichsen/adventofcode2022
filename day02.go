26ab517c8bf6bedbff97f761598cb602aaea217a
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2022-12-06 22:26:49 +0100

    feat: day 2 part 2

--- day02.go
+++ day02.go
@@ -5,12 +5,12 @@ const (
 	paper    = 2
 	scissors = 3
 
-	draw = 3
 	lose = 0
+	draw = 3
 	win  = 6
 )
 
-var scores = map[string]int{
+var scoresPart1 = map[string]int{
 	// opponent draws rock
 	"A X": rock + draw,
 	"A Y": paper + win,
@@ -27,7 +27,28 @@ var scores = map[string]int{
 	"C Z": scissors + draw,
 }
 
-func Day02(lines []string) int {
+var scoresPart2 = map[string]int{
+	// opponent draws rock
+	"A X": lose + scissors,
+	"A Y": draw + rock,
+	"A Z": win + paper,
+
+	// opponent draws paper
+	"B X": lose + rock,
+	"B Y": draw + paper,
+	"B Z": win + scissors,
+
+	// opponent draws scissors
+	"C X": lose + paper,
+	"C Y": draw + scissors,
+	"C Z": win + rock,
+}
+
+func Day02(lines []string, part1 bool) int {
+	scores := scoresPart2
+	if part1 {
+		scores = scoresPart1
+	}
 	sum := 0
 	for _, line := range lines {
 		score := scores[line]
--- day02_test.go
+++ day02_test.go
@@ -8,7 +8,7 @@ func TestDay02Part1Example(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day02(lines)
+	got := Day02(lines, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -20,19 +20,44 @@ func TestDay02Part1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day02(lines)
+	got := Day02(lines, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func BenchmarkDay02Part1(b *testing.B) {
+// Benchmark is identical for part 1 and part 2 (same algorithm)
+func BenchmarkDay02(b *testing.B) {
 	lines, err := linesFromFilename(filename(2))
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = Day02(lines)
+		_ = Day02(lines, true)
+	}
+}
+
+func TestDay02Part2Example(t *testing.T) {
+	const want = 12
+	lines, err := linesFromFilename(exampleFilename(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day02(lines, false)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay02Part2(t *testing.T) {
+	const want = 14979
+	lines, err := linesFromFilename(filename(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day02(lines, false)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
 	}
 }
