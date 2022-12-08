f4d913203e086783e05795bba20eea2a5a2deba0
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2022-12-08 11:33:17 +0100

    feat: day 4 part 1

---
+++ day04.go
@@ -0,0 +1,49 @@
+package adventofcode2021
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func Day04(lines []string) (int, error) {
+	parse := func(line string) (int, int, int, int, error) {
+		var err error
+		l0 := strings.Split(line, ",")
+		l1 := strings.Split(l0[0], "-")
+		l2 := strings.Split(l0[1], "-")
+		ss := []string{l1[0], l1[1], l2[0], l2[1]}
+		var ns [4]int
+		for i := range ns {
+			ns[i], err = strconv.Atoi(ss[i])
+			if err != nil {
+				msg := "error parsing number %q: %w"
+				err = fmt.Errorf(msg, ss[i], err)
+				break
+			}
+		}
+		return ns[0], ns[1], ns[2], ns[3], err
+	}
+
+	count := 0
+	for i, line := range lines {
+		x1, x2, x3, x4, err := parse(line)
+		if err != nil {
+			return 0, fmt.Errorf("error parsing line %d: %w",
+				i+1, err)
+		}
+		if Contains(x1, x2, x3, x4) {
+			log.Printf("contained: [%d-%d] [%d-%d]\n", x1, x2, x3, x4)
+			count++
+		}
+	}
+	return count, nil
+}
+
+// Contains returns true if the range a [a1..a2] is within b [b1..b2] or vice
+// versa.
+func Contains(a1, a2, b1, b2 int) bool {
+	return a1 >= b1 && a2 <= b2 || // a within b
+		a1 <= b1 && a2 >= b2 // b within a
+}
---
+++ day04_test.go
@@ -0,0 +1,63 @@
+package adventofcode2021
+
+import (
+	"fmt"
+	"testing"
+)
+
+var containsTests = []struct {
+	a1, a2, b1, b2 int
+	want           bool
+}{
+	{1, 4, 2, 3, true},
+	{1, 1, 1, 1, true},
+	{1, 1, 1, 3, true},
+	{1, 3, 1, 1, true},
+	{1, 1, 2, 2, false},
+	{2, 2, 1, 1, false},
+	{0, 0, 1, 9, false},
+	{1, 9, 0, 0, false},
+}
+
+func TestContains(t *testing.T) {
+	for _, tt := range containsTests {
+		id := fmt.Sprintf("[%d-%d] <-> [%d-%d]", tt.a1, tt.a2,
+			tt.b1, tt.b2)
+		t.Run(id, func(t *testing.T) {
+			got := Contains(tt.a1, tt.a2, tt.b1, tt.b2)
+			if tt.want != got {
+				t.Fatalf("want %t but got %t", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestDay04Example(t *testing.T) {
+	const want = 2
+	lines, err := linesFromFilename(exampleFilename(4))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day04(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay04(t *testing.T) {
+	const want = 500 // too high
+	lines, err := linesFromFilename(filename(4))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day04(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
