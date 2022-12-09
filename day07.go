184459bd23ca89880d50373a790eeca1eadb4fa8
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2022-12-09 23:01:13 +0100

    refactor: cleanup

--- day07.go
+++ day07.go
@@ -1,7 +1,6 @@
 package adventofcode2022
 
 import (
-	"fmt"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -19,39 +18,24 @@ func Day07(lines []string) int {
 		switch ss[0] {
 		case "$": // command
 			if ss[1] == "cd" {
-				fmt.Printf("wd cd %s\n", ss[2])
 				if ss[2][0] == '/' {
 					wd = ss[2]
 				} else {
 					wd = filepath.Join(wd, ss[2])
-					fmt.Printf("wd before clean %s\n", wd)
 					wd = filepath.Clean(wd)
-					fmt.Printf("wd after clean %s\n", wd)
 				}
-				fmt.Printf("wd switching to %s\n", wd)
 			}
 		case "dir": // directory
 			// record directories so that intermediate directories
 			// without files are accounted for
 			dirs[filepath.Join(wd, ss[1])] = true
 		default: // filesize
-			n, err := strconv.Atoi(ss[0])
-			if err != nil {
-				panic("bad")
-			}
+			n, _ := strconv.Atoi(ss[0])
 			f := filepath.Join(wd, ss[1])
-			fmt.Printf("wd registering %s\n", f)
 			sizes[f] = n
 			dirs[wd] = true
-			fmt.Printf("file %s: %d\n", f, n)
 		}
 	}
-
-	/*
-		for k := range dirs {
-			fmt.Printf("unique %s: \n", k)
-		}
-	*/
 
 	// calculate directory sizes
 	ds := make(map[string]int, len(dirs))
@@ -66,7 +50,6 @@ func Day07(lines []string) int {
 	// filter and sum
 	sum := 0
 	for _, size := range ds {
-		// fmt.Printf("%s: %d\n", dir, size)
 		if size <= limit {
 			sum += size
 		}
