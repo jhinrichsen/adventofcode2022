9a9964c5ff8b98c5e3207f6f6c59707bc7885a18
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2023-01-03 18:29:40 +0100

    feat: day 8 part 2

--- day08.go
+++ day08.go
@@ -1,8 +1,16 @@
 package adventofcode2022
 
-func Day08(lines []string) int {
+func Day08(lines []string, part1 bool) int {
+	if part1 {
+		return day08Part1(lines)
+	}
+	return day08Part2(lines)
+}
+
+func day08Part1(lines []string) int {
 	// for sparse fields i like to use reals as index, this field is full
 	// no clue for any advanced stuff
+	//
 	// in case of doubt choose brute force
 	// -- me
 
@@ -50,4 +58,74 @@ func Day08(lines []string) int {
 	// outside are always visible
 	outside := 2*X + 2*Y - 4 // 4 corners
 	return count + outside
+}
+
+func day08Part2(lines []string) int {
+	var (
+		X              = len(lines[0])
+		Y              = len(lines)
+		maxScenicScore = 0
+		height         = func(x, y int) int {
+			return int(lines[y][x] - '0')
+		}
+	)
+	for y := 0; y < Y; y++ {
+		for x := 0; x < X; x++ {
+			var cn int
+			h0 := height(x, y)
+			// north
+			for y2 := y - 1; ; y2-- {
+				if y2 < 0 {
+					break
+				}
+				cn++
+				h := height(x, y2)
+				if h >= h0 {
+					break
+				}
+			}
+			// east
+			var ce int
+			for x2 := x + 1; ; x2++ {
+				if x2 >= X {
+					break
+				}
+				ce++
+				h := height(x2, y)
+				if h >= h0 {
+					break
+				}
+			}
+			// south
+			var cs int
+			for y2 := y + 1; ; y2++ {
+				if y2 >= Y {
+					break
+				}
+				cs++
+				h := height(x, y2)
+				if h >= h0 {
+					break
+				}
+			}
+			// west
+			var cw int
+			for x2 := x - 1; ; x2-- {
+				if x2 < 0 {
+					break
+				}
+				cw++
+				h := height(x2, y)
+				if h >= h0 {
+					break
+				}
+			}
+
+			scenicScore := cn * ce * cs * cw
+			if maxScenicScore < scenicScore {
+				maxScenicScore = scenicScore
+			}
+		}
+	}
+	return maxScenicScore
 }
--- day08_test.go
+++ day08_test.go
@@ -2,25 +2,49 @@ package adventofcode2022
 
 import "testing"
 
-func TestDay08Example(t *testing.T) {
+func TestDay08Part1Example(t *testing.T) {
 	const want = 21
 	lines, err := linesFromFilename(exampleFilename(8))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day08(lines)
+	got := Day08(lines, true)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay08(t *testing.T) {
+func TestDay08Part1(t *testing.T) {
 	const want = 1835
 	lines, err := linesFromFilename(filename(8))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day08(lines)
+	got := Day08(lines, true)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay08Part2Example(t *testing.T) {
+	const want = 8
+	lines, err := linesFromFilename(exampleFilename(8))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day08(lines, false)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay08Part2(t *testing.T) {
+	const want = 263670
+	lines, err := linesFromFilename(filename(8))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day08(lines, false)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
